7991c7f6d4fc5f1a6d9450f1ad16c2f93c087575
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-13 17:47:14 +0100

    [WIP] Add initial setup for async rendering of embeds

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -4,11 +4,13 @@ namespace VanOns\Laraberg\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Embed\Embed;
+use Spatie\Async\Pool;
 
 use VanOns\Laraberg\Events\ContentRendered;
 
 class Content extends Model {
   protected $table = 'lb_contents';
+  public $embeds = [];
 
   public function contentable() {
     return $this->morphTo();
@@ -34,9 +36,31 @@ class Content extends Model {
    */
   public function renderRaw($raw) {
     $html = $raw;
-    $html = $this->renderEmbeds($html);
+    $html = $this->renderEmbedsParallel($html);
     event(new ContentRendered($this));
     return $html;
+  }
+  
+  public function renderEmbedsParallel($html) {
+    $pool = Pool::create();
+    $regex = '/<!-- wp:core-embed\/.*?--><figure class="wp-block-embed.*?".*?<div class="wp-block-embed__wrapper">(.*?)<\/div><\/figure>/';
+    preg_match_all($regex, $html, $matches);
+    // Get all embeds asynchronuosly
+    foreach ($matches[1] as $match) {
+      $pool->add(function () use ($match) {
+        return Embed::create($match)->code;
+      })->then(function($embed) {
+        array_push($this->embeds, $embed);
+      });
+    }
+    $pool->wait();
+    $index = 0;
+    $result = preg_replace_callback($regex, function($matches) use($index) {
+      $embed = $this->embeds[$index];
+      $index++;
+      return str_replace('>'.$matches[1].'<', '>'.$embed.'<', $matches[0]);
+    }, $html);
+    return $result;
   }
 
   public function renderBlocks($html) {
@@ -60,7 +84,8 @@ class Content extends Model {
   }
 
   public function renderEmbeds($html) {
-    $result = preg_replace_callback('/<!-- wp:core-embed\/.*?--><figure class="wp-block-embed.*?".*?<div class="wp-block-embed__wrapper">(.*?)<\/div><\/figure>/', function($matches) {
+    $regex = '/<!-- wp:core-embed\/.*?--><figure class="wp-block-embed.*?".*?<div class="wp-block-embed__wrapper">(.*?)<\/div><\/figure>/';
+    $result = preg_replace_callback($regex, function($matches) {
       $embed = Embed::create($matches[1])->code;
       return str_replace('>'.$matches[1].'<', '>'.$embed.'<', $matches[0]);
     }, $html);
